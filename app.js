29be0dd74c7d66905d68a6882140360027ee14a1
shimjaedoo <woen1637@naver.com>
2023-08-07 23:41:13 +0900

    댓글CRUD / utills폴더를 만들고 에러핸들링을 위해 customClass파일 만듬 / service로직에서 CustomError로 에러핸들링

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cookieParser = require('cookie-parser');
-const {} = require('./routes');
+const commentsRouter = require('./routes/card_detail.route');
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [, , , ,]);
+app.use('/api', [commentsRouter]);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
