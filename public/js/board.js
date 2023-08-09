a18803439996dbd690fd3f916f4d2f5d3a8b0e50
Yoonzero <playlist808@kakao.com>
2023-08-09 21:10:41 +0900

    column 조회 front DB test OK

--- public/js/board.js
+++ public/js/board.js
@@ -3,14 +3,59 @@ const board = document.querySelector('.board');
 // create-column button tag
 const columnBtn = document.querySelector('#columnBtn');
 
-// 컬럼생성 버튼 클릭 시 이벤트리스너 호출
-columnBtn.addEventListener('click', async () => {
+// 컬럼 조회
+document.addEventListener('DOMContentLoaded', async (e) => {
+  // boardId
   const params = new URLSearchParams(window.location.search);
   const boardId = params.get('boardId');
-  const columnName = prompt('생성할 컬럼명을 입력해주세요.');
-
+  // accessToken
   const accessToken = localStorage.getItem('accessToken');
+  // 컬럼조회 API fetch
+  try {
+    const getColumnResponse = await fetch(`/api/boards/${boardId}/columns`, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    // fetch로 받아온 data 가공
+    await getColumnResponse.json().then((result) => {
+      result.data.forEach((data) => {
+        // HTML 세팅
+        const columnSet = `
+                          <div class="column" id="${data.columnName}-column">
+                            <h2 class="column-title">${data.columnName}</h2>
+                            <div class="card">Card 1</div>
+                            <div class="card">Card 2</div>
+                            <button class="add-card-button">Add Card</button>
+                            <button class="delete-column-button">delete</button>
+                          </div>
+                          `;
+        // 화면에 HTML 띄우기
+        board.insertBefore(document.createRange().createContextualFragment(columnSet), columnBtn);
+      });
+      // 오류 출력
+      if (result.errorMessage) {
+        return alert(result.errorMessage);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    return alert('컬럼 조회에 실패하였습니다.');
+  }
+});
 
+// 컬럼 생성
+columnBtn.addEventListener('click', async () => {
+  // boardId
+  const params = new URLSearchParams(window.location.search);
+  const boardId = params.get('boardId');
+  // accessToken
+  const accessToken = localStorage.getItem('accessToken');
+  // 생성 columnName
+  const columnName = prompt('생성할 컬럼명을 입력해주세요.');
+  // 컬럼생성 API fetch
   try {
     const createResponse = await fetch(`/api/boards/${boardId}/columns`, {
       method: 'POST',
@@ -20,9 +65,9 @@ columnBtn.addEventListener('click', async () => {
       },
       body: JSON.stringify({ columnName }),
     });
-
+    // fetch로 받아온 data 가공
     await createResponse.json().then((result) => {
-      // 생성할 컬럼 HTML 세팅
+      // HTML 세팅
       const columnSet = `
                         <div class="column" id="${result.data.columnName}-column">
                           <h2 class="column-title">${result.data.columnName}</h2>
@@ -32,9 +77,9 @@ columnBtn.addEventListener('click', async () => {
                           <button class="delete-column-button">delete</button>
                         </div>
                         `;
-
+      // HTML 화면에 띄우기
       board.insertBefore(document.createRange().createContextualFragment(columnSet), columnBtn);
-
+      // 오류 출력
       result.errorMessage ? alert(result.errorMessage) : alert(`${columnName}으로 컬럼이 생성되었습니다.`);
     });
   } catch (err) {
@@ -44,13 +89,12 @@ columnBtn.addEventListener('click', async () => {
 });
 
 // 컬럼명 변경
-const titles = document.querySelectorAll('.column-title');
-// 각각의 h2태그에 addEventListener 호출
-titles.forEach((title) => {
+const h2Tag = document.querySelectorAll('.column-title');
+h2Tag.forEach((title) => {
   title.addEventListener('click', (e) => {
-    const accessToken = localStorage.getItem('accessToken');
     // 현재 사용중인 제목 변수처리
     const useTitle = title.textContent;
+    console.log(useTitle); // OK
     // 각 제목의 html 형식을 input 태그로 바꿔주고, value 값에 할당 변수 사용
     title.innerHTML = `<input type="text" value="${useTitle}" class="edit-input">`;
 
@@ -58,7 +102,7 @@ titles.forEach((title) => {
     inputTag.focus();
     // 커서위치 맨 뒤로
     inputTag.selectionStart = useTitle.length;
-
+    // 포커스를 벗어날 시 기존 제목으로 변경
     inputTag.addEventListener('blur', (e) => {
       // console.log(e.target);
       // if (e.target !== inputTag) {
@@ -73,7 +117,9 @@ titles.forEach((title) => {
       const boardId = params.get('boardId');
       // columnId
       const columnId = e.target.parentNode.parentNode.id;
-
+      // accessToken
+      const accessToken = localStorage.getItem('accessToken');
+      // 컬럼명 변경 API fetch
       try {
         if (e.key === 'Enter') {
           const columnName = inputTag.value;
@@ -86,9 +132,11 @@ titles.forEach((title) => {
             },
             body: JSON.stringify({ columnName }),
           });
-
+          // fetch로 받아온 data 가공
           await changeColumnNameResponse.json().then((result) => {
+            h2Tag.innerHTML = `<h2 class="column-title">${columnName}</h2>`;
             return result.errorMessage ? alert(result.errorMessage) : alert(result.message);
+            // window.location.reload();
           });
         }
       } catch (err) {
@@ -104,13 +152,14 @@ titles.forEach((title) => {
 const deleteButtons = document.querySelectorAll('.delete-column-button');
 deleteButtons.forEach((deleteBtn) => {
   deleteBtn.addEventListener('click', async (e) => {
-    const accessToken = localStorage.getItem('accessToken');
     // boardId
     const params = new URLSearchParams(window.location.search);
     const boardId = params.get('boardId');
     // columnId
     const columnId = e.target.parentNode.id;
-
+    // accessToken
+    const accessToken = localStorage.getItem('accessToken');
+    // 컬럼삭제 API fetch
     try {
       const deleteResponse = await fetch(`/api/boards/${boardId}/columns/${columnId}`, {
         method: 'DELETE',
@@ -119,6 +168,7 @@ deleteButtons.forEach((deleteBtn) => {
           'Content-Type': 'application/json',
         },
       });
+      // fetch로 받아온 data 가공
       await deleteResponse.json().then((result) => {
         result.errorMessage ? alert(result.errorMessage) : alert(result.message);
       });
